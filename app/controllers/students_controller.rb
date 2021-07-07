ce31be9ab1f46a9836af9f6128dba70620f7977e
Brinton <brintonfoyreed@gmail.com>
2021-07-07 13:01:36 -0700

    commit tree 0702d2793d76b3ad975c701240cdb2107e8f08df

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,15 @@
-class StudentsController < ApplicationController
 
+ class StudentsController < ApplicationController
   def index
-    students = Student.all
+    # byebug
+    p("****")
+    p(params)
+    students = Student.find_by(first_name: params[:first_name], last_name: params[:last_name], grade: params[:grade])
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
